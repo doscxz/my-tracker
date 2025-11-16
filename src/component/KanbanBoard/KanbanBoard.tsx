0a23e693f2ddeacb673e2970d2228bf3afbac67e
k <k@m.com>
2025-11-16 16:02:24 +0300

    Измерение памяти redux

--- src/component/KanbanBoard/KanbanBoard.tsx
+++ src/component/KanbanBoard/KanbanBoard.tsx
@@ -53,6 +53,28 @@ const KanbanBoard = () => {
       // }
       // const end = performance.now();
       // console.log('Время выполнения 1000 задач', end - start); // 370.39999997615814
+
+      // const initialMemory = performance.memory.usedJSHeapSize;
+      // const tasks = [];
+      // for (let i = 0; i < 1000; i++) {
+      //   const newTask = { ...task, title: task.title + i };
+      //   tasks.push(newTask);
+      //   dispatch(createTask(newTask));
+      // }
+      // // Принудительно ждем и предотвращаем оптимизацию
+      // await new Promise((resolve) => {
+      //   setTimeout(() => {
+      //     // Используем tasks чтобы предотвратить GC
+      //     console.log(tasks.length);
+      //     resolve();
+      //   }, 200);
+      // });
+
+      // const finalMemory = performance.memory.usedJSHeapSize;
+      // const memoryUsed = (finalMemory - initialMemory) / 1024 / 1024;
+      // console.log('Было занято памяти:', memoryUsed.toFixed(2), 'MB'); //Было занято памяти: 21.82 MB
+      // console.log('Начальная память:', (initialMemory / 1024 / 1024).toFixed(2), 'MB'); // Начальная память: 89.83 MB
+      // console.log('Конечная память:', (finalMemory / 1024 / 1024).toFixed(2), 'MB'); // Конечная память: 111.65 MB
     } catch (e) {
       console.error(e);
     } finally {
